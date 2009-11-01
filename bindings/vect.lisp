cb5ffe2cbc5bf21a039fef69dcf1591963772d20
Ralith <ralith@arbor.localdomain>
2009-10-31 20:58:02 -0700

    Added more vector related bindings.

--- bindings/vect.lisp
+++ bindings/vect.lisp
@@ -4,3 +4,14 @@
   "Simple two-dimensional vector"
   (x cp-float)
   (y cp-float))
+
+(defcfun ("cpvlength" vect-length) cp-float
+  (vector vect))
+(defcfun ("cpvlengthsq" vect-length-sq) cp-float
+  (vector vect))
+(defcfun ("cpvnormalize" vect-normalize) vect
+  (vector vect))
+(defcfun ("cpvforangle" angle->vect) vect
+  (angle cp-float))
+(defcfun ("cpvtoangle" vect->angle) cp-float
+  (vector vect))
